fd543cf7e9335f3bff2a092c94f7c6d287fb520e
TrolulArdelean <trolulardeleanoriginal@gmail.com>
2022-12-16 00:42:40 +0200

    Added sample file, changed gitignore

--- app/src/main/java/com/transactionmonitor/MainActivity.kt
+++ app/src/main/java/com/transactionmonitor/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.transactionmonitor
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onBtnClick(view: View) {
+        val greetingText = findViewById<TextView>(R.id.textView)
+        val username = findViewById<EditText>(R.id.editTextTextPersonName)
+        greetingText.text = username.text.toString()
     }
 }
