f36166071f00753694f216b9dd861990f65f40ee
TrolulArdelean <trolulardeleanoriginal@gmail.com>
2022-12-19 21:14:24 +0200

    Added example registration

--- app/src/main/java/com/transactionmonitor/MainActivity.kt
+++ app/src/main/java/com/transactionmonitor/MainActivity.kt
@@ -12,9 +12,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun onBtnClick(view: View) {
-        val greetingText = findViewById<TextView>(R.id.textView)
-        val username = findViewById<EditText>(R.id.editTextTextPersonName)
-        greetingText.text = username.text.toString()
+    fun onRegisterBtnClick(view: View) {
+        val firstName = findViewById<EditText>(R.id.textFirstName)
+        val lastName = findViewById<EditText>(R.id.textLastName)
+        val email = findViewById<EditText>(R.id.textEmail)
+
+        val errorTooltip = findViewById<TextView>(R.id.errorTooltip)
+        val errorMessage = "Error: the parameter {} is required!"
+
+        if (firstName.text.isNullOrBlank()) {
+            errorTooltip.visibility = View.VISIBLE
+            errorTooltip.text = errorMessage.replace("{}", "First Name")
+        } else if (lastName.text.isNullOrBlank()) {
+            errorTooltip.visibility = View.VISIBLE
+            errorTooltip.text = errorMessage.replace("{}", "Last Name")
+        } else if (email.text.isNullOrBlank()) {
+            errorTooltip.visibility = View.VISIBLE
+            errorTooltip.text = errorMessage.replace("{}", "Email")
+        }
+        else {
+            errorTooltip.visibility = View.INVISIBLE
+            findViewById<TextView>(R.id.textSetFirstName).text = firstName.text
+            findViewById<TextView>(R.id.textSetLastName).text = lastName.text
+            findViewById<TextView>(R.id.textSetEmail).text = email.text
+        }
     }
 }
